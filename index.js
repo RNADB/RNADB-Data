a98ccdd396467394a3c0942f605dfc4b710db57e
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-19 00:35:19 +0530

    Initial commit
    Reading {./citation.json} from the —//root//— to get the citation of — RNADB®.
    Note: Removing byte order marker.
    SEED™: Grunt based build system. | See: http://seed.sequomics.com/.

--- index.js
+++ index.js
@@ -80,3 +80,10 @@ function stripBOM(content) {
   }
   return content;
 }
+
+///--------------------
+// A citation of RNADB®
+///--------------------
+var cite = JSON.parse(stripBOM(fs.readFileSync('./citation.json', {
+  encoding: "utf8"
+})));
